c3a310b37a26acffc7a59431b2545f3d09b6cd7b
Peleke <peleke@trilogyed.com>
2017-07-08 13:07:55 -0400

    :art: Add explanatory comment to POST route.

--- helpers.js
+++ helpers.js
@@ -3,3 +3,8 @@ const path = require('path')
 exports.sendFile = function sendFile (res, filename) {
   res.sendFile(path.join(__dirname, filename))
 }
+
+exports.isValid = function isValid (table) {
+  // verify that table is valid--for now, just verify nonnullity
+  return !!table
+}
